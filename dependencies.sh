0b67c056cb3f4a04272294f2cdb5d1f4c75e3fd4
Simonas <20096648+simjak@users.noreply.github.com>
2024-07-17 10:12:57 +0300

    feat: added dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -3,4 +3,3 @@ sudo apt install python3-pip
 sudo apt-get install libgl1-mesa-glx
 sudo apt-get install -y poppler-utils
 sudo apt install tesseract-ocr
-pip install torch==2.2.1 torchvision==0.17.1 --index-url https://download.pytorch.org/whl/cpu
